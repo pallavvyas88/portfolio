bdac990e65c6614738699cd3c26eae39666a338c
Pallav Vyas <vyas.pallav8@gmail.com>
2026-09-24 14:44:03 +0530

    Revert "Merge pull request #8 from pallavvyas88/feature/doodle-illustration"
    This reverts commit c17fa51b0235ccfa1fa4ba9b4f2d6acf4bde1336, reversing
    changes made to aeb86c5a74e4bc3abbc84b2735c56726c9eb509b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import AnnouncementBar from '@/components/AnnouncementBar';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
-import IllustrationSection from '@/components/IllustrationSection';
 import About from '@/components/About';
 import Services from '@/components/Services';
 import Portfolio from '@/components/Portfolio';
@@ -29,9 +28,6 @@ export default function Home() {
       <main className="flex-1 space-y-4">
         {/* Hero Section */}
         <Hero />
-
-        {/* Feature Illustration */}
-        <IllustrationSection />
 
         {/* Engineering Philosophy & Skills */}
         <About />
